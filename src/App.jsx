57d79c6a8bbc61dbf57b190e78dc92638fba5a33
Thanh Trieu <zpeng2k@gmail.com>
2023-10-09 01:21:19 +0700

    11 setup theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
 import HomeIcon from "@mui/icons-material/Home";
 import { pink } from "@mui/material/colors";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <>
-      <div>truongtrieu</div>
+      <Typography variant="body1" color="primary.main">
+        truongtrieu
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
