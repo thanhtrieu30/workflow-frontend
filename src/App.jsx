315a3c3822599cbc7983d45fdf9d12366a759d4d
Thanh Trieu <zpeng2k@gmail.com>
2023-10-11 23:22:00 +0700

    13. Đồng bộ chuẩn Dark Mode & Light Mode

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,51 @@ import HomeIcon from "@mui/icons-material/Home";
 import { pink } from "@mui/material/colors";
 import Typography from "@mui/material/Typography";
 import { useColorScheme } from "@mui/material/styles";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import Box from "@mui/material/Box";
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="id-mode-select-light-dark">Mode</InputLabel>
+      <Select
+        labelId="label-mode-select-light-dark"
+        id="id-mode-select-light-dark"
+        value={mode}
+        label="mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <NightsStayIcon fontSize="small" /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -22,6 +67,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <hr />
       <Typography variant="body1" color="primary.main">
